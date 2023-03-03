4533fb48e9676863b2a33690a94da49fcc22ba10
PeterE2 <Melvinpedro4@gmail.com>
2023-03-03 17:45:42 -0300

    Mudanças no sistema

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,9 +6,14 @@ using TMPro;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField] private float spaceBtwCurriculum;
     [SerializeField] private GameObject contractObject;
     [SerializeField] private Transform contractPlacement;
     [SerializeField] private List<Contract> contracts;
+
+    private List<int> selected = new List<int>();
+
+    private int maxSelected = 4;
 
     private List<GameObject> contractObjects;
 
@@ -17,6 +22,7 @@ public class GameManager : MonoBehaviour
 
     private void Start()
     {
+        curIndex = Mathf.Clamp(Mathf.RoundToInt(contracts.Count / 2),0, contracts.Count - 1);
         contractObjects = new List<GameObject>();   
         foreach(Contract cont in contracts)
         {
@@ -35,14 +41,32 @@ public class GameManager : MonoBehaviour
             return;
         }
 
+        if(Input.GetKeyDown(KeyCode.R))
+        {
+            if(selected.Contains(curIndex))
+            {
+                selected.Remove(curIndex);
+                UpdateContracts();
+                return;
+            }
+
+            if(selected.Count < maxSelected)
+            {
+                selected.Add(curIndex);
+                UpdateContracts();
+            }
+
+            
+        }
+
         if(Input.GetKey(KeyCode.LeftArrow))
         {
-            SetCur(1);
+            SetCur(-1);
             timer = .15f;
         }
         if (Input.GetKey(KeyCode.RightArrow))
         {
-            SetCur(-1);
+            SetCur(1);
             timer = .15f;
         }
     }
@@ -62,10 +86,11 @@ public class GameManager : MonoBehaviour
             if (i == curIndex)
             {
                 Vector3 pos = contractPlacement.transform.position;
+                pos.y += (selected.Contains(i) ? .5f : .25f);
                 
                 curContract.LeanMove(pos,0.05f);
                 //curContract.transform.localScale = Vector3.one * 1.25f;
-
+                
                 curContract.GetComponentInChildren<SpriteRenderer>().sortingOrder = 1000;
                 curContract.GetComponentInChildren<TextMeshPro>().sortingOrder = 1000;
             }
@@ -74,10 +99,12 @@ public class GameManager : MonoBehaviour
                 //Debug.Log(());
                 Vector3 offset = Vector3.zero;
 
-                float offsetValue = 1f;
-                offsetValue *= (2 - (.15f * Mathf.Abs(i - curIndex)));
+                float offsetValue = spaceBtwCurriculum;
+                offsetValue *= Mathf.Max(3 - (.15f * Mathf.Abs(i - curIndex)),0f);
+                //Debug.Log(offsetValue);
 
-                offset.x = offsetValue * (i - curIndex);
+                offset.x = offsetValue * (i - curIndex) ;
+                offset.y = (selected.Contains(i) ? .5f : 0f);
                 curContract.GetComponentInChildren<SpriteRenderer>().sortingOrder = (-Mathf.Abs(Mathf.RoundToInt(1 - (curIndex - i))) );
                 curContract.GetComponentInChildren<TextMeshPro>().sortingOrder = (-Mathf.Abs(Mathf.RoundToInt(1 - (curIndex - i))) );
 
